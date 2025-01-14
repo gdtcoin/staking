49de93c848f8c47b59cb3275083320096f188d8a
gdtcoin <gdtc666888@163.com>
2025-01-14 17:30:11 +0800

    Update lib.rs

--- lib.rs
+++ lib.rs
@@ -638,11 +638,15 @@ pub mod gdtc_staking {
 
         // 检查奖励账户余额是否足够
         if gdtc_reward_out_account.amount < accumulated_reward {
-            user_instance.staked_info[index].can_cancel_stake = true;
-            user_instance.total_deposited_amount = user_instance
-                .total_deposited_amount
-                .checked_sub(user_instance.staked_info[index].deposited_amount)
-                .ok_or(ErrorCode::Overflow)?;
+            if current_timestamp >= user_instance.staked_info[index].stake_end_time
+                && user_instance.user_address == ctx.accounts.authority.key()
+            {
+                user_instance.staked_info[index].can_cancel_stake = true;
+                user_instance.total_deposited_amount = user_instance
+                    .total_deposited_amount
+                    .checked_sub(user_instance.staked_info[index].deposited_amount)
+                    .ok_or(ErrorCode::Overflow)?;
+            }
             return Ok(());
         }
 
